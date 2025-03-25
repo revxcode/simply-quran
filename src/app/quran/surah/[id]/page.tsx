c4d6ebb6c578b584788ffe7aabb7738f411a1e52
revxcode <akunpo5521@gmail.com>
2025-03-25 07:21:31 +0700

    Update Page: using Infinity Scroll technique

--- src/app/quran/surah/[id]/page.tsx
+++ src/app/quran/surah/[id]/page.tsx
@@ -1,25 +1,94 @@
+"use client"
+
+import type { Surah } from "@/types/Surah";
 import AudioPlayer from "@/components/audio/audioPlayer";
 import PrevNextSurah from "@/components/button/prevNextSurah";
 import MainLayout from "@/components/layouts/mainLayout";
-import { cache } from "react";
+import { useEffect, useRef, useState } from "react";
+import { AyatCard, AyatCardSkeleton } from "@/components/card/ayatCard";
 
-const fetchSurah = cache(async (id: number) => {
+const fetchSurah = async (id: number) => {
   const host = process.env.NEXT_PUBLIC_HOST_NAME;
-  console.log(host);
   const response = await fetch(`${host}/api/quran/surah/${id}`);
   const data = await response.json();
   return data;
-});
+};
 
-export default async function SurahPage({ params }: {
+const fetchAyat = async (id: number, s: number, n: number) => {
+  const host = process.env.NEXT_PUBLIC_HOST_NAME;
+  const response = await fetch(`${host}/api/quran/surah/${id}/ayat?start=${s}&end=${n}`);
+  const data = await response.json();
+  return data;
+};
+
+export default function SurahPage({ params }: {
   params: Promise<{ id: number }>
 }) {
-  const { id } = await params;
-  const data = await fetchSurah(id);
+  const [data, setData] = useState<Surah>({} as Surah);
+  const [loading, setLoading] = useState(true);
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastElementRef = useRef<HTMLDivElement | null>(null);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    params.then(async ({ id }) => {
+      const surah = await fetchSurah(id);
+      const ayat = await fetchAyat(id, 1, 10);
+      setData({
+        ...surah,
+        ayat: ayat,
+      });
+      setLoading(false);
+    });
+
+    if (!lastElementRef.current) return;
+
+    const loadMoreData = (entries: IntersectionObserverEntry[]) => {
+      const entry = entries[0];
+      if (entry.isIntersecting && !loading) {
+        // Jika elemen terakhir terlihat, ambil data berikutnya
+        setPage((prev) => prev + 1);
+      }
+    };
+
+    observer.current = new IntersectionObserver(loadMoreData, {
+    });
+
+    if (lastElementRef.current) {
+      observer.current.observe(lastElementRef.current);
+    }
+
+    // Bersihkan observer ketika komponen unmount
+    return () => {
+      if (observer.current && lastElementRef.current) {
+        observer.current.disconnect();
+      }
+    };
+
+  }, [loading]);
+
+  useEffect(() => {
+    if (page > 1) {
+      fetchAyat(data.nomor, (page - 1) * 10 + 1, page * 10).then((ayat) => {
+        setData({
+          ...data,
+          ayat: [...data.ayat, ...ayat],
+        });
+      });
+    }
+  }, [page]);
+
+  if (loading) {
+    return <div className="flex w-full min-h-screen items-center justify-center">
+      <span className="text-xl font-bold text-neutral-800">
+        Loading...
+      </span>
+    </div>;
+  }
 
   return (
     <MainLayout>
-      <div className="w-full h-full bg-white rounded-2xl p-8">
+      <div className="w-full h-fit bg-white rounded-2xl p-8">
         <div className="flex justify-between items-center">
           <span className="text-2xl font-bold text-emerald-400 bg-emerald-50 px-2 rounded-full">{data.nomor}</span>
           <span className="text-3xl font-bold text-neutral-800">{data.nama}</span>
@@ -36,26 +105,14 @@ export default async function SurahPage({ params }: {
         </div>
         <div className="flex flex-col gap-4 mt-8 py-8">
           {data.ayat.map((ayat: any) => (
-            <div key={ayat.nomor}>
-              <div className="flex flex-col gap-2 py-4">
-                <div className="flex justify-between w-full">
-                  <div className="w-8 h-8 bg-emerald-500 rounded-full flex justify-center items-center aspect-square">
-                    <span className="text-lg text-white font-bold">{ayat.nomor}</span>
-                  </div>
-                  <span className="text-4xl text-neutral-800 text-end py-2 px-2">{ayat.ar}</span>
-                </div>
-                <div className="flex w-full flex-col">
-                  <span className="text-md text-neutral-500 text-start py-2">{ayat.idn}</span>
-                </div>
-                <div className="w-full border-b border-neutral-200 my-4"></div>
-              </div>
-            </div>
+            <AyatCard key={ayat.nomor} ayat={ayat} />
           ))}
-          <PrevNextSurah
-            prev={data.nomor - 1}
-            next={data.nomor + 1}
-          />
         </div>
+        <AyatCardSkeleton ref={lastElementRef} />
+        <PrevNextSurah
+          prev={data.nomor - 1}
+          next={data.nomor + 1}
+        />
       </div >
     </MainLayout >
   );
